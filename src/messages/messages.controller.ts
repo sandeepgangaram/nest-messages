63f4983355af69f2b5bcfec2100efe9cda9940f0
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-01-05 15:16:36 +0530

    add dummy routes

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return 'AllMessages';
+  }
+
+  @Post()
+  createMessage() {
+    return 'Successfully added a message';
+  }
+
+  @Get('/:id')
+  getMessage() {
+    return 'Got specific message';
+  }
+}
