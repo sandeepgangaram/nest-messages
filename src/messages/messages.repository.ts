f8399060ebf6537c646c866d00daf9a971732623
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-01-16 15:14:53 +0530

    add message service

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+import { readFile, writeFile } from 'fs/promises';
+export class MessagesRepository {
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    return messages[id];
+  }
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    return messages;
+  }
+
+  async create(message: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, message };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
+}
